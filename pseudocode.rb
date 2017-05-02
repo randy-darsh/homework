94e9dddd061454eb2ed8d7726196a5790adfcede
rspringer47 <rspringer47@gmail.com>
2017-05-02 09:14:11 -0600

    first upload

---
+++ pseudocode.rb
@@ -0,0 +1,33 @@
+# BIG PICTURE: to try to manipulate/annoy the answer of the user to be "GOODBYE!" and then "GOODBYE!" again
+
+# the interface will be a simple prompt to enter text.
+
+# first it needs to prompt the users input.
+
+"HELLO, THIS IS A GROCERY STORE!"
+
+# user will input (most likely) at least 1 lowercase letter.
+
+# 1 lowercase letter returns:
+
+"I AM HAVING A HARD TIME HEARING YOU."
+
+# ALL CAPS will return
+
+"NO, THIS IS NOT A PET STORE."
+
+# and if they input nothing it will prompt them with a nice
+
+"HELLO?!"
+
+# at this point they will want to quit and to quit they must enter "GOODBYE!" twice in a row.
+
+# first response
+
+"ANYTHING ELSE I CAN HELP YOU WITH?"
+
+# second response
+
+"THANK YOU FOR CALLING!"
+
+end
